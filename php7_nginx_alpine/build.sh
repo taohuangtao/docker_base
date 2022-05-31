1422f70719ea76ac120bcdce5a100409dd23507b
huangtao <tao_huangtao@qq.com>
2022-05-31 11:32:47 +0800

    调整基础

--- php7_nginx_alpine/build.sh
+++ php7_nginx_alpine/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-docker build -t hub.pengbei.tech:18080/bigdata/php:php7.4-nginx-kafka-v1 .
-docker push hub.pengbei.tech:18080/bigdata/php:php7.4-nginx-kafka-v1
+docker build -t taohuangtao/php:php7.4-nginx-kafka-v2.1 .
+docker push taohuangtao/php:php7.4-nginx-kafka-v2.1
