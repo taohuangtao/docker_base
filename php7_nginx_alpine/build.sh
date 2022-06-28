c29140b78447959f1746353317137d3fec0c3922
huangtao <tao_huangtao@qq.com>
2022-06-28 17:48:52 +0800

    减少php错误输出

--- php7_nginx_alpine/build.sh
+++ php7_nginx_alpine/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
-docker build -t taohuangtao/php:php7.4-nginx-kafka-v2.1 .
-docker push taohuangtao/php:php7.4-nginx-kafka-v2.1
+V=v2.2
+docker build -t taohuangtao/php:php7.4-nginx-kafka-$V .
+docker push taohuangtao/php:php7.4-nginx-kafka-$V
