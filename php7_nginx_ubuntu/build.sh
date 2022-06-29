912a3462dd7b83bf7d31eaa0092e383e8b7eb443
huangtao <tao_huangtao@qq.com>
2022-06-29 10:48:31 +0800

    调整基础镜像

--- php7_nginx_ubuntu/build.sh
+++ php7_nginx_ubuntu/build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 # - 后面表示有更新，应该使用最新的
 V=7.4-3
-L=7.4
+L=7.4-latest
 docker build -t taohuangtao/nginx-php:${V} .
 docker push     taohuangtao/nginx-php:${V}
 docker tag      taohuangtao/nginx-php:${V} taohuangtao/nginx-php:$L
